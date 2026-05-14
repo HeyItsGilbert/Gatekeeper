cdca943a9c8f7be34c888ce931595affcd358571
Gilbert Sanchez <me@gilbertsanchez.com>
2026-05-13 17:11:25 -0700

    fix(Test-Condition): remove dead variable $validation

--- Gatekeeper/Public/Test-Condition.ps1
+++ Gatekeeper/Public/Test-Condition.ps1
@@ -88,7 +88,6 @@ function Test-Condition {
 
     $meta = $PropertySet.GetProperty($propName)
     $propType = $meta.Type
-    $validation = $meta.Validation
 
     $actual = Convert-ToTypedValue -Type $propType -Value $Context[$propName]
     $valid = $meta.Validate($actual)
